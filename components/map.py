bae211f2d9e5e2041209ad91cf5ae4f65c526be2
UpLong23 <panos-makris@outlook.com>
2026-06-05 12:08:37 +0300

    Almost final

--- components/map.py
+++ components/map.py
@@ -116,4 +116,4 @@ def render_risk_map(parcels, risks, center_lat=55.913, center_lon=13.107,
 </script>"""
         map_html = map_html.replace("</body>", script + "</body>")
 
-    st.iframe(map_html, width=None, height=500)
+    st.iframe(map_html, width='stretch', height=500)
